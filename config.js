04af081a0fdbdafb87ccebb090af86050da7979c
Bartłomiej Trzonek <barthek7@gmail.com>
2018-08-20 17:22:56 +0200

    Fix authentication problems

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '954667063636-g1qag37uh84lms2q2ifvqepp4eqh71nj.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '_5WZDl0QhYO3XaHhxIGtc5If',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'    
+}
